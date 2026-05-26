ba8ac2ff4f1abb5e4ac3fc428a494009dc58aa49
isabelabastos45-dotcom <isabela.bastos45@aluno.ifce.edu.br>
2026-05-26 17:44:53 -0300

    inicio avaliacao 8

---
+++ avaliacoes/avaliacao-08/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liiAAo3Ms9tCTcwN';
+const githubClientSecret = '2dafb5eb25bb5300ed6f9640b37d82c143b86ae6';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
