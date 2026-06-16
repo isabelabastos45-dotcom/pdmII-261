0f76d66fa6623d2a8743b01b7b7b6eddd3eeafc5
isabelabastos45-dotcom <isabela.bastos45@aluno.ifce.edu.br>
2026-06-16 16:55:39 -0300

    avaliacao 7

---
+++ avaliacoes/avaliacao-07/flutter-codelabs/flutter_ci_script_main.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+DIR="${BASH_SOURCE%/*}"
+source "$DIR/flutter_ci_script_shared.sh"
+
+declare -a CODELABS=(
+  "animated-responsive-layout"
+  "animations"
+  "brick_breaker"
+  "colorist"
+  "dart-patterns-and-records"
+  "deeplink_cookbook"
+  "firebase-auth-flutterfire-ui"
+  "firebase-get-to-know-flutter"
+  "generate_crossword"
+  "genui-intro"
+  "google-maps-in-flutter"
+  "homescreen_codelab"
+  "in_app_purchases"
+  "namer"
+  "tooling"
+  "webview_flutter"
+  )
+
+ci_codelabs "main" "${CODELABS[@]}"
+
+echo "== END OF TESTS"
